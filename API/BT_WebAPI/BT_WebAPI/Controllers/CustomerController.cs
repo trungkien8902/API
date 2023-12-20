621d3ec56882a6e36f71698a791233047397e461
Trung Kien <trungkienlx02@gmail.com>
2023-12-20 17:42:20 +0700

    .
    .

---
+++ API/BT_WebAPI/BT_WebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace BT_WebAPI.Controllers
+{
+    [EnableCors(origins: "https://localhost:44320", headers: "*", methods: "*")]
+    public class TestController : ApiController
+    {
+        // Controller methods not shown...
+    }
+    public class CustomerController : ApiController
+    {
+        //httpGet: Dùng để lấy thông tin khách hàng
+        //1. Dịch vụ lấy thông tin của toàn bộ khách hàng
+        [HttpGet]
+        public List<tblKhach> GetCustomerLists()
+        {
+            DataDataContext dbCustomer = new DataDataContext();
+            return dbCustomer.tblKhaches.ToList();
+        }
+        //2. Dịch vụ lấy thông tin khách hàng với 1 mã nào đó
+
+        [HttpGet]
+        public tblKhach GetCustomer(string id)
+        {
+            DataDataContext dbCustomer = new DataDataContext();
+            return dbCustomer.tblKhaches.FirstOrDefault(x => x.Makhach == id);
+        }
+
+        //3. httpPost, thêm mới một khách hàng
+        [HttpPost]
+        public bool InsertCustomer(string id, string name, string address, string phoneNumber)
+        {
+            try
+            {
+                DataDataContext dbCustomer = new DataDataContext();
+                tblKhach customer = new tblKhach();
+                customer.Makhach = id;
+                customer.TenKhach = name;
+                customer.DiaChi = address;
+                customer.DienThoai = phoneNumber;
+                dbCustomer.tblKhaches.InsertOnSubmit(customer);
+                dbCustomer.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        //4. httpPut để chỉnh sửa thông tin khách hàng
+
+        [HttpPut]
+        public bool UpdateCustomer(string id, string name, string address, string phoneNumber)
+        {
+            try
+            {
+                DataDataContext dbCustomer = new DataDataContext();
+                //Lấy mã khách
+                tblKhach customer = dbCustomer.tblKhaches.FirstOrDefault(x => x.Makhach == id);
+                if(customer == null) return false;
+                customer.Makhach = id;
+                customer.TenKhach = name;
+                customer.DiaChi = address;
+                customer.DienThoai = phoneNumber;
+                dbCustomer.SubmitChanges(); //Xác nhận chỉnh sửa
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        //5. httpDelete để xóa 1 khách hàng
+        [HttpDelete]
+        public bool DeleteCustomer(string id)
+        {
+            try
+            {
+                DataDataContext dbCustomer = new DataDataContext();
+                //Lấy mã khách hàng cần xóa
+                tblKhach customer = dbCustomer.tblKhaches.FirstOrDefault(x => x.Makhach == id);
+                if(customer == null) return false;
+                dbCustomer.tblKhaches.DeleteOnSubmit(customer);
+                dbCustomer.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+    }
+}
---
+++ API/BT_WebAPI/BT_WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BT_WebAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        [HttpGet]
+        public List<tblHang> GetSanPhams()
+        {
+            DataDataContext db = new DataDataContext();
+            return db.tblHangs.ToList();
+        }
+        [HttpGet]
+        public tblHang GetProduct(string id)
+        {
+            DataDataContext db = new DataDataContext();
+            return db.tblHangs.FirstOrDefault(n => n.Mahang == id);
+        }
+        [Route("Search/{name}")]
+        [HttpGet]
+        public List<tblHang> GetSanPham(string name)
+        {
+            DataDataContext db = new DataDataContext();
+            return db.tblHangs.Where(n => n.TenHang.Contains(name)).ToList();
+        }
+        [Route("TonKho")]
+        [HttpGet]
+        public List<tblHang> GetTonKho()
+        {
+            DataDataContext db = new DataDataContext();
+            return db.tblHangs.Where(n => n.SoLuong > 0).ToList();
+        }
+        [HttpPost]
+        public bool InsertNewProduct(string id, string name, string description, int PriceIn, int PriceOut, int number)
+        {
+            try
+            {
+                DataDataContext dBProduct = new DataDataContext();
+                tblHang product = new tblHang();
+                product.Mahang = id;
+                product.TenHang = name;
+                product.GhiChu = description;
+                product.DonGiaNhap = PriceIn;
+                product.DonGiaBan = PriceOut;
+                product.SoLuong = number;
+                dBProduct.tblHangs.InsertOnSubmit(product);
+                dBProduct.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        [HttpPut]
+        public bool UpdateProduct(string id, string name, string description, int PriceIn, int PriceOut, int number)
+        {
+            try
+            {
+                DataDataContext dBProduct = new DataDataContext();
+                tblHang product = dBProduct.tblHangs.FirstOrDefault(n => n.Mahang == id);
+                if (product == null) { return false; }
+                product.Mahang = id;
+                product.TenHang = name;
+                product.GhiChu = description;
+                product.DonGiaNhap = PriceIn;
+                product.DonGiaBan = PriceOut;
+                product.SoLuong = number;
+                dBProduct.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        [HttpDelete]
+        public bool DeleteProduct(string id)
+        {
+            try
+            {
+                DataDataContext dBProduct = new DataDataContext();
+                tblHang product = dBProduct.tblHangs.FirstOrDefault(n => n.Mahang == id);
+                if (product == null) { return false; }
+                dBProduct.tblHangs.DeleteOnSubmit(product);
+                dBProduct.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
